cb46782afd6e9f716e4b51638bc938ed3d1656cd
Patri22k <bajzik.patrik6@gmail.com>
2024-11-17 20:01:12 +0100

    Start working again wohoo, added whole structure of a code, restructure code to new functions like addTask() and listAllTasks() and so on

--- main.go
+++ main.go
@@ -1,25 +1,156 @@
 package main
 
 import (
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
-func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+type Task struct {
+	ID          int    // json:"id"
+	Description string // json:"description"
+	Status      string // json:"status"
+	/*createAt
+	updateAt    string json:"updateAt"*/
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	// Check if the user has provided any command
+	if os.Args[1] == "add" && len(os.Args) == 3 {
+
+		// ADD
+		task := Task{
+			ID:          generateID(),
+			Description: os.Args[2],
+			Status:      "pending",
+		}
+
+		// Check if the ID was generated correctly
+		if task.ID == -1 {
+			fmt.Println("Error generating the ID")
+			return
+		}
+
+		// Marshaling the text to JSON
+		taskJSON, err := json.Marshal(task)
+		if err != nil {
+			fmt.Println("Error marshaling the task")
+			return
+		}
+
+		// Write the JSON to a file
+		filename := "tasks.json"
+		err = os.WriteFile(filename, taskJSON, 0644)
+		if err != nil {
+			fmt.Println("Error writing the task to the file")
+			return
+		}
+
+		// Test
+		fmt.Printf("Task written to the file: %s %s\n", filename, taskJSON)
+		return
+	} else if os.Args[1] == "update" && len(os.Args) == 4 {
+
+		// UPDATE
+
+	} else if os.Args[1] == "delete" && len(os.Args) == 3 {
+
+		// DELETE
+
+	} else if os.Args[1] == "mark-in-progress" && len(os.Args) == 3 {
+
+		// MARK IN-PROGRESS
+
+	} else if os.Args[1] == "mark-done" && len(os.Args) == 3 {
+
+		// MARK DONE
+
+	} else if os.Args[1] == "list" && len(os.Args) == 2 {
+
+		// LIST ALL TASKS
+		data, err := os.ReadFile("tasks.json")
+		if err != nil {
+			fmt.Println("Error reading the file")
+			return
+		}
+
+		// Unmarshaling the JSON to a struct
+		var readTask Task
+		err = json.Unmarshal(data, &readTask)
+		if err != nil {
+			fmt.Println("Error unmarshaling the task")
+			return
+		}
+
+		// Test
+		fmt.Printf("Task read from the file: %v\n", readTask)
+
+	} else if os.Args[1] == "list" && os.Args[2] == "done" {
+
+		// LIST DONE
+
+	} else if os.Args[1] == "list" && os.Args[2] == "todo" {
+
+		// TODO LIST
+
+	} else if os.Args[1] == "list" && os.Args[2] == "in-progress" {
+
+		// LIST IN-PROGRESS
+
+	} else if os.Args[1] == "help" && len(os.Args) == 2 {
+
+		// HELP
+		fmt.Println("Commands:")
+		fmt.Println("add <description>")
+		fmt.Println("update <id> <description>")
+		fmt.Println("delete <id>")
+		fmt.Println("mark-in-progress <id>")
+		fmt.Println("mark-done <id>")
+		fmt.Println("list")
+		fmt.Println("list done")
+		fmt.Println("list todo")
+		fmt.Println("list in-progress")
+
+	} else {
+
+		// ERROR
+		fmt.Println("Please write a valid command. For help write 'help'")
+		return
+	}
+}
+
+func generateID() int {
+	filename := "tasks.json"
+
+	// Check if the file exists
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		// If the file does not exist, start the ID from 1
+		return 1
+	}
+
+	// Read the file
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading the file", err)
+		return -1
+	}
+
+	// Unmarshaling the JSON to a struct
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		fmt.Println("Error unmarshaling the tasks", err)
+		return -1
+	}
+
+	// Find the highest ID
+	highestID := 0
+	for _, task := range tasks {
+		if task.ID > highestID {
+			highestID = task.ID
+		}
+	}
+
+	// Return the highest ID + 1
+	return highestID + 1
+}
